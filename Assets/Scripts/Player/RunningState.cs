c45e1f9250843f9154de28051cb8b821efc2c882
Kuba M <kuba.maciejowski27@gmail.com>
2025-07-14 15:39:36 +0200

    refactor: implement player state machine using interface and separate state classes

--- Assets/Scripts/Player/RunningState.cs
+++ Assets/Scripts/Player/RunningState.cs
@@ -1,28 +1,28 @@
-// using UnityEngine;
+using UnityEngine;
 
-// public class RunningState : IPlayerState
-// {
+public class RunningState : IPlayerState
+{
 
-//  public void Enter(PlayerMovement player)
-//     {
+ public void Enter(PlayerMovement player)
+    {
         
-//     }
+    }
 
-//     public void Update(PlayerMovement player)
-//     {
+    public void Update(PlayerMovement player)
+    {
         
 
-//         if (player.moveInput == Vector2.zero || player.rb.linearVelocity.magnitude < 0.05f)
-//         {
-//             player.changeState(new IdleState());
-//         }
+        if (player.moveInput == Vector2.zero || player.rb.linearVelocity.magnitude < 0.05f)
+        {
+            player.changeState(new IdleState());
+        }
 
        
-//     }
+    }
 
-//     public void Exit(PlayerMovement player)
-//     {
+    public void Exit(PlayerMovement player)
+    {
         
-//     }
+    }
 
-// }
+}
