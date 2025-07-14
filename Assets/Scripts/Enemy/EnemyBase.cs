c4e114a1bd37e342b46ad97ae146d83119e163b6
Kuba M <kuba.maciejowski27@gmail.com>
2025-07-14 15:30:06 +0200

    Pierwszy commit dodajemy wszystkie pliki

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+
+    [Header("Stats")]
+    [SerializeField] protected int startingHealth = 100;
+    [SerializeField] protected float movementSpeed = 4;
+    [SerializeField] protected float attackRange = 1.5f;
+    [SerializeField] protected int enemyDamage = 10;
+    [SerializeField] protected float cooldownAttack = 2f;
+
+    [Header("Inspector Attributes")]
+    [SerializeField]protected Transform player;
+    protected HealthSystem healthSystem;
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    protected virtual void Awake()
+    {
+        healthSystem = GetComponent<HealthSystem>();
+        healthSystem.Init();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    protected virtual void EnemyDie()
+    {
+        Debug.Log("Enemy Died");
+    }
+
+    protected virtual void TakeDamage(int amount)
+    {
+        healthSystem.ChangeHealth(-enemyDamage);
+        if (healthSystem.isDead())
+        {
+            EnemyDie();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/RunningState.cs
@@ -0,0 +1,28 @@
+// using UnityEngine;
+
+// public class RunningState : IPlayerState
+// {
+
+//  public void Enter(PlayerMovement player)
+//     {
+        
+//     }
+
+//     public void Update(PlayerMovement player)
+//     {
+        
+
+//         if (player.moveInput == Vector2.zero || player.rb.linearVelocity.magnitude < 0.05f)
+//         {
+//             player.changeState(new IdleState());
+//         }
+
+       
+//     }
+
+//     public void Exit(PlayerMovement player)
+//     {
+        
+//     }
+
+// }
